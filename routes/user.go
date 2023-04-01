a538b63972c3176fd9646a413ecc1b7ddeb0f561
Fehlling Halim <kevnhalim@gmail.com>
2023-04-02 06:53:00 +0700

    fix: remove trailing slash from routes

--- routes/user.go
+++ routes/user.go
@@ -11,11 +11,11 @@ import (
 func UserRoutes(router *gin.Engine, userC controller.UserController) {
 	userRoutes := router.Group("/api/v1/users")
 	{
-		userRoutes.GET("/", middleware.Authenticate(service.NewJWTService(), "admin"), userC.GetAllUsers)
+		userRoutes.GET("", middleware.Authenticate(service.NewJWTService(), "admin"), userC.GetAllUsers)
 		userRoutes.GET("/:username", middleware.Authenticate(service.NewJWTService(), "user"), userC.GetUserByUsername)
 		userRoutes.PUT("/name", middleware.Authenticate(service.NewJWTService(), "user"), userC.UpdateSelfName)
-		userRoutes.DELETE("/", middleware.Authenticate(service.NewJWTService(), "user"), userC.DeleteSelfUser)
-		userRoutes.POST("/", userC.Register)
+		userRoutes.DELETE("", middleware.Authenticate(service.NewJWTService(), "user"), userC.DeleteSelfUser)
+		userRoutes.POST("", userC.Register)
 		userRoutes.POST("/login", userC.Login)
 	}
 }
