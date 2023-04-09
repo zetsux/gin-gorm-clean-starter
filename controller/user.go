6eb6acc0788508eaee46eaa897b59f9c9cf063a8
Fehlling Halim <kevnhalim@gmail.com>
2023-04-09 12:26:59 +0700

    refactor: adjust responses

--- controller/user.go
+++ controller/user.go
@@ -37,7 +37,7 @@ func (userC *userController) Register(ctx *gin.Context) {
 	var userDTO dto.UserRegisterRequest
 	err := ctx.ShouldBind(&userDTO)
 	if err != nil {
-		resp := common.CreateFailResponse("failed to process user register request", http.StatusBadRequest)
+		resp := common.CreateFailResponse("Failed to process user register request", http.StatusBadRequest)
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, resp)
 		return
 	}
@@ -45,7 +45,7 @@ func (userC *userController) Register(ctx *gin.Context) {
 	// Check for duplicate Username or Email
 	userCheck, err := userC.userService.GetUserByUsernameOrEmail(ctx, userDTO.Username, userDTO.Email)
 	if err != nil {
-		resp := common.CreateFailResponse("failed to process user register request", http.StatusBadRequest)
+		resp := common.CreateFailResponse("Failed to process user register request", http.StatusBadRequest)
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, resp)
 		return
 	}
@@ -54,11 +54,11 @@ func (userC *userController) Register(ctx *gin.Context) {
 	if !(reflect.DeepEqual(userCheck, entity.User{})) {
 		var resp common.Response
 		if userCheck.Username == userDTO.Username && userCheck.Email == userDTO.Email {
-			resp = common.CreateFailResponse("username and email are already used", http.StatusBadRequest)
+			resp = common.CreateFailResponse("Username and Email are already used", http.StatusBadRequest)
 		} else if userCheck.Username == userDTO.Username {
-			resp = common.CreateFailResponse("username is already used", http.StatusBadRequest)
+			resp = common.CreateFailResponse("Username is already used", http.StatusBadRequest)
 		} else if userCheck.Email == userDTO.Email {
-			resp = common.CreateFailResponse("email is already used", http.StatusBadRequest)
+			resp = common.CreateFailResponse("Email is already used", http.StatusBadRequest)
 		}
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, resp)
 		return
@@ -66,12 +66,12 @@ func (userC *userController) Register(ctx *gin.Context) {
 
 	newUser, err := userC.userService.CreateNewUser(ctx, userDTO)
 	if err != nil {
-		resp := common.CreateFailResponse("failed to process user register request", http.StatusBadRequest)
+		resp := common.CreateFailResponse("Failed to process user register request", http.StatusBadRequest)
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, resp)
 		return
 	}
 
-	resp := common.CreateSuccessResponse("successfully registered user", http.StatusCreated, newUser)
+	resp := common.CreateSuccessResponse("Successfully registered user", http.StatusCreated, newUser)
 	ctx.JSON(http.StatusCreated, resp)
 }
 
@@ -79,44 +79,44 @@ func (userC *userController) Login(ctx *gin.Context) {
 	var userDTO dto.UserLoginRequest
 	err := ctx.ShouldBind(&userDTO)
 	if err != nil {
-		resp := common.CreateFailResponse("failed to process user login request", http.StatusBadRequest)
+		resp := common.CreateFailResponse("Failed to process user login request", http.StatusBadRequest)
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, resp)
 		return
 	}
 
 	res := userC.userService.VerifyLogin(ctx.Request.Context(), userDTO.UserIdentifier, userDTO.Password)
 	if !res {
-		response := common.CreateFailResponse("entered credentials invalid", http.StatusBadRequest)
+		response := common.CreateFailResponse("Entered credentials invalid", http.StatusBadRequest)
 		ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
 		return
 	}
 
 	user, err := userC.userService.GetUserByIdentifier(ctx.Request.Context(), userDTO.UserIdentifier)
 	if err != nil {
-		response := common.CreateFailResponse("failed to process user login request", http.StatusBadRequest)
+		response := common.CreateFailResponse("Failed to process user login request", http.StatusBadRequest)
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, response)
 		return
 	}
 
 	token := userC.jwtService.GenerateToken(user.ID, user.Role)
 	authResp := common.CreateAuthResponse(token, user.Role)
-	resp := common.CreateSuccessResponse("user login successful", http.StatusOK, authResp)
+	resp := common.CreateSuccessResponse("User login successful", http.StatusOK, authResp)
 	ctx.JSON(http.StatusOK, resp)
 }
 
 func (userC *userController) GetAllUsers(ctx *gin.Context) {
 	users, err := userC.userService.GetAllUsers(ctx)
 	if err != nil {
-		resp := common.CreateFailResponse("failed to fetch all users", http.StatusBadRequest)
+		resp := common.CreateFailResponse("Failed to fetch all users", http.StatusBadRequest)
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, resp)
 		return
 	}
 
 	var resp common.Response
 	if len(users) == 0 {
-		resp = common.CreateSuccessResponse("no user found", http.StatusOK, users)
+		resp = common.CreateSuccessResponse("No user found", http.StatusOK, users)
 	} else {
-		resp = common.CreateSuccessResponse("successfully fetched all users", http.StatusOK, users)
+		resp = common.CreateSuccessResponse("Successfully fetched all users", http.StatusOK, users)
 	}
 	ctx.JSON(http.StatusOK, resp)
 }
@@ -125,16 +125,16 @@ func (userC *userController) GetUserByUsername(ctx *gin.Context) {
 	username := ctx.Param("username")
 	user, err := userC.userService.GetUserByIdentifier(ctx, username)
 	if err != nil {
-		resp := common.CreateFailResponse(err.Error(), http.StatusBadRequest)
+		resp := common.CreateFailResponse("Failed to fetch user", http.StatusBadRequest)
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, resp)
 		return
 	}
 
 	var resp common.Response
 	if reflect.DeepEqual(user, entity.User{}) {
-		resp = common.CreateSuccessResponse("user not found", http.StatusOK, nil)
+		resp = common.CreateSuccessResponse("User not found", http.StatusOK, nil)
 	} else {
-		resp = common.CreateSuccessResponse("successfully fetched user", http.StatusOK, user)
+		resp = common.CreateSuccessResponse("Successfully fetched user", http.StatusOK, user)
 	}
 	ctx.JSON(http.StatusOK, resp)
 }
@@ -143,16 +143,16 @@ func (userC *userController) GetMe(ctx *gin.Context) {
 	id := ctx.GetUint64("ID")
 	user, err := userC.userService.GetUserByID(ctx, id)
 	if err != nil {
-		resp := common.CreateFailResponse("failed to fetch user", http.StatusBadRequest)
+		resp := common.CreateFailResponse("Failed to fetch user", http.StatusBadRequest)
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, resp)
 		return
 	}
 
 	var resp common.Response
 	if reflect.DeepEqual(user, entity.User{}) {
-		resp = common.CreateSuccessResponse("user not found", http.StatusOK, nil)
+		resp = common.CreateSuccessResponse("User not found", http.StatusOK, nil)
 	} else {
-		resp = common.CreateSuccessResponse("successfully fetched user", http.StatusOK, user)
+		resp = common.CreateSuccessResponse("Successfully fetched user", http.StatusOK, user)
 	}
 	ctx.JSON(http.StatusOK, resp)
 }
@@ -161,7 +161,7 @@ func (userC *userController) UpdateSelfName(ctx *gin.Context) {
 	var userDTO dto.UserNameUpdateRequest
 	err := ctx.ShouldBind(&userDTO)
 	if err != nil {
-		resp := common.CreateFailResponse("failed to process user name update request", http.StatusBadRequest)
+		resp := common.CreateFailResponse("Failed to process user name update request", http.StatusBadRequest)
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, resp)
 		return
 	}
@@ -176,9 +176,9 @@ func (userC *userController) UpdateSelfName(ctx *gin.Context) {
 
 	var resp common.Response
 	if reflect.DeepEqual(user, entity.User{}) {
-		resp = common.CreateSuccessResponse("user not found", http.StatusOK, nil)
+		resp = common.CreateSuccessResponse("User not found", http.StatusOK, nil)
 	} else {
-		resp = common.CreateSuccessResponse("successfully updated user", http.StatusOK, user)
+		resp = common.CreateSuccessResponse("Successfully updated user", http.StatusOK, user)
 	}
 	ctx.JSON(http.StatusOK, resp)
 }
@@ -187,11 +187,11 @@ func (userC *userController) DeleteSelfUser(ctx *gin.Context) {
 	id := ctx.GetUint64("ID")
 	err := userC.userService.DeleteSelfUser(ctx, id)
 	if err != nil {
-		resp := common.CreateFailResponse(err.Error(), http.StatusBadRequest)
+		resp := common.CreateFailResponse("Failed to delete user", http.StatusBadRequest)
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, resp)
 		return
 	}
 
-	resp := common.CreateSuccessResponse("successfully deleted user", http.StatusOK, nil)
+	resp := common.CreateSuccessResponse("Successfully deleted user", http.StatusOK, nil)
 	ctx.JSON(http.StatusOK, resp)
 }
